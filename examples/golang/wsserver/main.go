40a08e8f5541f5549e1ff19291090506027d18f4
mirahs <mirahs@163.com>
2021-08-22 18:22:02 +0800

    golang websocket 服务端示例优化

--- examples/golang/wsserver/main.go
+++ examples/golang/wsserver/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"golang.org/x/net/websocket"
 	"io"
 	"net/http"
-	"os"
 	"packet"
-	packetutil "packet/util"
 	"server/proto"
 )
 
@@ -16,16 +15,16 @@ type WSServer struct {
 	ListenAddr string
 }
 
-func (this *WSServer) start() error {
-	http.Handle("/websocket", websocket.Handler(this.handler))
+
+func (this *WSServer) start() {
 	fmt.Println("begin to listen")
+
+	http.Handle("/websocket", websocket.Handler(this.handler))
+
 	err := http.ListenAndServe(this.ListenAddr, nil)
 	if err != nil {
-		fmt.Println("ListenAndServe:", err)
-		return err
+		panic(err)
 	}
-	fmt.Println("start end")
-	return nil
 }
 
 func (this *WSServer) handler(conn *websocket.Conn) {
@@ -33,63 +32,46 @@ func (this *WSServer) handler(conn *websocket.Conn) {
 
 	fmt.Printf("a new ws conn: %s->%s\n", conn.RemoteAddr().String(), conn.LocalAddr().String())
 
-	headLen := 2
-	buffers := make([]byte, 51200)
-	buffersLen := 0
-	buffersTmp := make([]byte, 512)
-
 	for {
-		readLen, err := conn.Read(buffersTmp)
-		if err != nil {
-			if err == io.EOF {
-				fmt.Fprintf(os.Stderr, "Client exit: %s\n", conn.RemoteAddr())
-			} else {
-				fmt.Fprintf(os.Stderr, "Read error: %s\n", err)
-			}
+		headData := make([]byte, 2)
+		_, err := io.ReadFull(conn, headData)
+		if flag := checkNetError(err); !flag {
 			return
 		}
 
-		copy(buffers[buffersLen:], buffersTmp[:readLen])
-		buffersLen += readLen
-
-		for {
-			if buffersLen > headLen {
-				bodyLen := int(packetutil.ReadU16(buffers))
-				if buffersLen >= headLen + bodyLen {
-					bodyBuff := buffers[headLen:bodyLen+headLen]
-					buffers = buffers[headLen+bodyLen:]
-					buffersLen -= int(bodyLen) + headLen
-
-					dispatch(conn, bodyBuff)
-				} else {
-					break
-				}
-			} else {
-				break
-			}
+		bodyLen := binary.BigEndian.Uint16(headData)
+		bodyData := make([]byte, bodyLen)
+		_, err = io.ReadFull(conn, bodyData)
+		if flag := checkNetError(err); !flag {
+			return
 		}
+
+		dispatch(conn, bodyData)
 	}
 }
 
 
-func main(){
+func main() {
 	wsServer := &WSServer{
-		ListenAddr : "127.0.0.1:8889",
+		ListenAddr: "127.0.0.1:8889",
 	}
 	wsServer.start()
+
 	fmt.Println("------end-------")
 }
 
 
-func dispatch(conn *websocket.Conn, bodyBuff []byte) {
-	packetIdBuff := bodyBuff[:2]
-	packetId := packetutil.ReadU16(packetIdBuff)
-	println("packetId: ", packetId)
-	buf := bodyBuff[2:]
-	packetN := packet.NewReadBuff(buf)
+func dispatch(conn *websocket.Conn, bodyData []byte) {
+	packetIdData := bodyData[:2]
+	packetId := binary.BigEndian.Uint16(packetIdData)
+	println("packetId:", packetId)
+
+	packetData := bodyData[2:]
+	packetInst := packet.NewReadBuff(packetData)
+
 	switch packetId {
 	case proto.P_ROLE_LOGIN:
-		roleLogin := proto.RoleLoginDecode(packetN)
+		roleLogin := proto.RoleLoginDecode(packetInst)
 		fmt.Println("roleLogin:", roleLogin)
 
 		goodsItems := []*proto.GoodsItem{{Id: 100, Num: 11}, {Id: 200, Num: 22}}
@@ -101,4 +83,17 @@ func dispatch(conn *websocket.Conn, bodyBuff []byte) {
 	default:
 		fmt.Println("unknown packetId:", packetId)
 	}
+}
+
+
+func checkNetError(err error) bool {
+	if err != nil {
+		if err == io.EOF {
+			fmt.Println("client exited")
+		} else {
+			fmt.Println("client error:", err)
+		}
+		return false
+	}
+	return true
 }
